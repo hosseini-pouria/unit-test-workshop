293d221009bb278284ec0ccf9fecaf9e1f1febf3
Kapouria <seydpooryahoseini@gmail.com>
2024-11-23 17:41:50 +0330

    Add price and quntity api

--- app/Product/Product.php
+++ app/Product/Product.php
@@ -8,10 +8,10 @@ class Product
     public const INVALID_QUANTITY_MESSAGE = 'موجودی محصول نمی تواند کمتر از صفر باشد';
 
     public function __construct(
-        public readonly string $title,
-        public readonly int $price,
-        public readonly int $quantity,
-        public readonly string $category)
+        public string $title,
+        public int $price,
+        public int $quantity,
+        public string $category)
     {
         if ($this->price < 0) {
             throw new \InvalidArgumentException(self::INVALID_PRICE_MESSAGE);
@@ -19,5 +19,38 @@ class Product
         if ($this->quantity < 0) {
             throw new \InvalidArgumentException(self::INVALID_QUANTITY_MESSAGE);
         }
+    }
+
+    public function changePrice(int $newPrice): void
+    {
+        if ($newPrice < 0) {
+            throw new \InvalidArgumentException(self::INVALID_PRICE_MESSAGE);
+        }
+
+        $this->price = $newPrice;
+    }
+
+    public function makeAsFree(): void
+    {
+        $this->changePrice(0);
+    }
+
+    public function changeQuantity(int $newQuantity): void
+    {
+        if ($newQuantity < 0) {
+            throw new \InvalidArgumentException(self::INVALID_QUANTITY_MESSAGE);
+        }
+
+        $this->quantity = $newQuantity;
+    }
+
+    public function decreaseQuantityByOne(): void
+    {
+        $this->changeQuantity($this->quantity--);
+    }
+
+    public function increaseQuantityByOne(): void
+    {
+        $this->changeQuantity($this->quantity++);
     }
 }
--- tests/Product/ProductTest.php
+++ tests/Product/ProductTest.php
@@ -2,8 +2,10 @@
 
 namespace Tests\Product;
 
+use App\Models\User;
 use App\Product\Product;
 use Illuminate\Foundation\Testing\WithFaker;
+use PHPUnit\Framework\Attributes\DataProvider;
 use Tests\TestCase;
 
 class ProductTest extends TestCase
@@ -75,4 +77,92 @@ class ProductTest extends TestCase
         }
     }
 
+    #[DataProvider('priceList')]
+    public function test_changing_price(int $newPrice): void
+    {
+        // arrange
+        $title = $this->faker->word();
+        $price = $this->faker->numberBetween(100_000,500_000);
+        $quantity = $this->faker->numberBetween(10,100);;
+        $category = $this->faker->word();
+        $sut = new Product($title,$price,$quantity,$category);
+        // $user = User::factory()->make(); // Laravel example
+
+        // act
+        $sut->changePrice($newPrice);
+
+        // assert
+        $this->assertEquals($newPrice, $sut->price);
+    }
+
+    public function test_it_not_possible_to_change_price_with_negative_price(): void
+    {
+        // arrange
+        $title = $this->faker->word();
+        $price = $this->faker->numberBetween(100_000,500_000);
+        $quantity = $this->faker->numberBetween(10,100);;
+        $category = $this->faker->word();
+        $sut = new Product($title,$price,$quantity,$category);
+        $newPrice = -2_000_000;
+
+        try {
+            // act
+            $sut->changePrice($newPrice);
+        } catch (\Exception $exception) {
+            // assert
+            $this->assertInstanceOf(\InvalidArgumentException::class, $exception);
+            $this->assertEquals(Product::INVALID_PRICE_MESSAGE, $exception->getMessage());
+        }
+    }
+
+    #[DataProvider('quantityList')]
+    public function test_changing_quantity(int $newQuantity)
+    {
+        // arrange
+        $title = $this->faker->word();
+        $price = $this->faker->numberBetween(100_000,500_000);
+        $quantity = $this->faker->numberBetween(10,100);;
+        $category = $this->faker->word();
+        $sut = new Product($title,$price,$quantity,$category);
+
+        // act
+        $sut->changeQuantity($newQuantity);
+
+        // assert
+        $this->assertEquals($newQuantity, $sut->quantity);
+    }
+
+    public function test_its_not_possible_to_change_quantity_with_negative_value(): void
+    {
+        // arrange
+        $title = $this->faker->word();
+        $price = $this->faker->numberBetween(100_000,500_000);
+        $quantity = $this->faker->numberBetween(10,100);;
+        $category = $this->faker->word();
+        $sut = new Product($title,$price,$quantity,$category);
+        $newQuantity = -10;
+
+        // assert
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage(Product::INVALID_QUANTITY_MESSAGE);
+
+        // act
+        $sut->changeQuantity($newQuantity);
+    }
+
+    public static function priceList(): array
+    {
+        return[
+            'مقدار یک میلیون تومان' => [1_000_000],
+            'مقدار صفر' => [0]
+        ];
+    }
+
+    public static function quantityList(): array
+    {
+        return[
+            'تعداد 60' => [60],
+            'تعداد صفر' => [0]
+        ];
+    }
 }

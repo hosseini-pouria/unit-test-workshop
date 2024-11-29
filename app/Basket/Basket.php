fd479502c800f51fc1e201f00fe8f8761249739a
hosseini-pouria <seydpooryahoseini@gmail.com>
2024-11-29 14:45:59 +0330

    Done basket calss

--- app/Basket/Basket.php
+++ app/Basket/Basket.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Collection;
 
 class Basket
 {
+    public const PRODUCT_NOT_FOUND_ERR_MESSAGE = 'محصول مورد نظر یافت نشد';
     private Collection $items;
 
     public function __construct()
@@ -16,18 +17,52 @@ class Basket
 
     public function addProduct(Product $product): void
     {
+        if ($this->items->has((string)$product->productId)){
+            $basketItem = $this->items->get((string)$product->productId);
+            $basketItem['quantity']++;
+            $this->items->put((string)$product->productId, $basketItem);
+            return;
+        }
+
         $this->items->put((string)$product->productId, ['quantity' => 1, 'product' => $product]);
     }
 
-    public function count(): int
+    public function itemsCount(): int
     {
         return $this->items->count();
     }
 
-    public function totalPrice(): void
+    public function totalPrice(): int
     {
-        return ;
+        return $this->items->reduce(fn(int $total, array $basketItem) =>
+                $total += $basketItem['product']->price() * $basketItem['quantity'], 0
+            );
     }
 
+    public function removeProduct(Product $product): void
+    {
+        $this->items = $this->items->forget($product->productId);
+    }
+
+    public function decreasingProductQuantity(Product $product): void
+    {
+        if (!$this->items->has((string)$product->productId)){
+            throw new \RuntimeException(self::PRODUCT_NOT_FOUND_ERR_MESSAGE);
+        }
+
+        $basketItem = $this->items->get((string)$product->productId);
+        if ($basketItem['quantity'] == 1){
+            $this->removeProduct($product);
+            return;
+        }
+
+        $basketItem['quantity']--;
+        $this->items->put((string)$product->productId, $basketItem);
+    }
+
+    public function deleted(): void
+    {
+        $this->items = collect([]);
+    }
 
 }
--- tests/Basket/BasketTest.php
+++ tests/Basket/BasketTest.php
@@ -18,7 +18,7 @@ class BasketTest extends TestCase
         $sut->addProduct($product);
 
         // assert
-        $this->assertEquals(1, $sut->count());
+        $this->assertEquals(1, $sut->itemsCount());
     }
 
     public function test_add_a_duplicate_product_increases_quantity(): void
@@ -27,14 +27,13 @@ class BasketTest extends TestCase
         $product = ProductBuilder::aProduct()->build();
         $sut = new Basket();
         $sut->addProduct($product);
-        $expectedQuantity = 2;
 
         // act
         $sut->addProduct($product);
 
         // assert
-        $this->assertEquals(1, $sut->count());
-      //  $this->assertEquals($expectedQuantity, );
+        $this->assertEquals(1, $sut->itemsCount());
+        $this->assertEquals($product->price() * 2 , $sut->totalPrice());
     }
 
     public function test_calculate_total_price(): void
@@ -49,5 +48,62 @@ class BasketTest extends TestCase
 
         // assert
         $this->assertEquals($product->price(), $totalPrice);
+    }
+
+    public function test_remove_product()
+    {
+        // arrange
+        $product = ProductBuilder::aProduct()->build();
+        $sut = new Basket();
+        $sut->addProduct($product);
+
+        // act
+        $sut->removeProduct($product);
+
+        // assert
+        $this->assertEquals(0, $sut->itemsCount());
+    }
+
+    public function test_decreasing_product_quantity(): void
+    {
+        // arrange
+        $product = ProductBuilder::aProduct()->build();
+        $sut = new Basket();
+        $sut->addProduct($product);
+        $sut->addProduct($product);
+
+        // act
+        $sut->decreasingProductQuantity($product);
+
+        // assert
+        $this->assertEquals($product->price(), $sut->totalPrice());
+    }
+
+    public function test_its_not_possible_to_decrease_product_quantity_when_it_dose_not_exists_in_the_basket(): void
+    {
+        // arrange
+        $product = ProductBuilder::aProduct()->build();
+        $sut = new Basket();
+
+        // assert
+        $this->expectException(\RuntimeException::class);
+        $this->expectExceptionMessage(Basket::PRODUCT_NOT_FOUND_ERR_MESSAGE);
+
+        // act
+        $sut->decreasingProductQuantity($product);
+    }
+
+    public function test_deleting_basket(): void
+    {
+        // arrange
+        $product = ProductBuilder::aProduct()->build();
+        $sut = new Basket();
+        $sut->addProduct($product);
+
+        // act
+        $sut->deleted();
+
+        // arrange
+        $this->assertEquals(0, $sut->itemsCount());
     }
 }
